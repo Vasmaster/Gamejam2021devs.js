5473adedbd6f3b0e29c0e0e423e2ca6af1273228
patrykcwirko <37484838+patrykcwirko@users.noreply.github.com>
2021-04-20 01:58:59 +0200

    Implement basic movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,16 +5,34 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private GameObject frontPlayer;
+    [SerializeField] private GameObject sidePlayer;
+
     public float speed;
-    private Vector2 _moveDir;
+    private Rigidbody2D frontPlayerRB;
+    private Rigidbody2D sidePlayerRB;
+    private float _moveDir;
+    private float _verticalDir;
+
+    private void Awake()
+    {
+        frontPlayerRB = frontPlayer.GetComponent<Rigidbody2D>();
+        sidePlayerRB = sidePlayer.GetComponent<Rigidbody2D>();
+    }
 
     private void Update()
     {
-        transform.Translate(_moveDir* speed * Time.deltaTime);
+        frontPlayerRB.velocity = transform.right * Time.deltaTime * _moveDir * speed;
+        sidePlayerRB.velocity = new Vector2(_moveDir, _verticalDir) * Time.deltaTime * speed;
     }
 
-    public void OnMove(InputAction.CallbackContext obj)
+    public void OnLeftRight(InputAction.CallbackContext obj)
     {
-        _moveDir = obj.ReadValue<Vector2>();
+        _moveDir = obj.ReadValue<float>();
+    }
+
+    public void OnUpDown(InputAction.CallbackContext obj)
+    {
+        _verticalDir = obj.ReadValue<float>();
     }
 }
